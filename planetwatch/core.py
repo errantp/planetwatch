9ff9b45a91b2e733bd5c000afd89f9abf5634a6a
Errantp <>
2021-09-13 15:37:07 -0700

    Update api

--- planetwatch/core.py
+++ planetwatch/core.py
@@ -47,7 +47,9 @@ class Wallet(object):
         prices = cg.get_coin_market_chart_by_id(
             id="planetwatch", vs_currency=currency, days=89
         )["prices"]
-        prices = pd.DataFrame(prices, columns=["timestamp", f"purchase_price_{currency}"])
+        prices = pd.DataFrame(
+            prices, columns=["timestamp", f"purchase_price_{currency}"]
+        )
         data = pd.to_datetime(prices.timestamp, unit="ms")
         prices["date"] = data.dt.date
         prices["hour"] = data.dt.hour
@@ -61,29 +63,49 @@ class Wallet(object):
         ]
         return current_prices
 
+    def get_cost(self, currency):
+        transactions = self.get_non_zero_transactions()
+        prices = self.get_prices(currency=currency)
+        results = transactions[["amount", "date"]][transactions.reward == True].merge(
+            prices[[f"purchase_price_{currency}", "date"]]
+        )
+        current_price = Wallet.get_current_price()[currency]
+        results[f"current_value_{currency}"] = (
+            Wallet.get_current_price()[currency] * results["amount"]
+        )
+        results[f"purchase_value_{currency}"] = (
+            results[f"purchase_price_{currency}"] * results["amount"]
+        )
+        results[f"gain_{currency}"] = (
+            results[f"current_value_{currency}"] - results[f"purchase_value_{currency}"]
+        )
+        return results, current_price
+
 
 @click.command()
-@click.option('--wallet', help='Planet Wallet', required=True)
-@click.option('--currency', default='usd', help='Currency to convert planets into.')
-@click.option('--csv', is_flag=True, help="Export csv of all transactions for given wallet")
-
-
+@click.option("--wallet", help="Planet Wallet", required=True)
+@click.option("--currency", default="usd", help="Currency to convert planets into.")
+@click.option(
+    "--csv", is_flag=True, help="Export csv of all transactions for given wallet"
+)
+@click.option("--dashboard", is_flag=True, help="Launch dashboard")
 def cli(currency, csv, wallet):
-    wallet_address = wallet
-    wallet = Wallet(wallet_address=wallet_address)
-    transactions = wallet.get_non_zero_transactions()
-    prices = wallet.get_prices(currency=currency)
-    results = transactions[["amount", "date"]][transactions.reward == True].merge(
-        prices[[f"purchase_price_{currency}", "date"]]
-    )
-    current_price = Wallet.get_current_price()[currency]
-    results[f'current_value_{currency}'] = Wallet.get_current_price()[currency]*results["amount"]
-    results[f'purchase_value_{currency}'] = results[f'purchase_price_{currency}']*results["amount"]
-    results[f"gain_{currency}"] = results[f'current_value_{currency}'] - results[f'purchase_value_{currency}']
+
+    planet_wallet = Wallet(wallet_address=wallet)
+    results, current_price = planet_wallet.get_cost(currency)
 
     print(f"The current price in {currency} is : {current_price}")
-    print(results.sum()[["amount", f'current_value_{currency}', f"purchase_value_{currency}", f"gain_{currency}"]])
+    print(
+        results.sum()[
+            [
+                "amount",
+                f"current_value_{currency}",
+                f"purchase_value_{currency}",
+                f"gain_{currency}",
+            ]
+        ]
+    )
     print(results.head(10))
 
     if csv:
-        results.to_csv(f"{wallet_address}.csv", index=False)
+        results.to_csv(f"{wallet}.csv", index=False)
